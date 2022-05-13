8a08809a79d110f4b77be070e8b89fa3cc9ef6c8
ylorph <ylorph@users.noreply.github.com>
2022-05-13 19:37:57 +0200

    exercise-projections

--- application/available_slots_projection_v2.go
+++ application/available_slots_projection_v2.go
@@ -13,29 +13,14 @@ type AvailableSlotsProjectionV2 struct {
 func NewAvailableSlotsProjectionV2(r *mongodb.AvailableSlotsRepositoryV2) *AvailableSlotsProjectionV2 {
 	p := infrastructure.NewEventHandler()
 	p.When(events.SlotScheduled{}, func(e interface{}, m infrastructure.EventMetadata) error {
-		s := e.(events.SlotScheduled)
-		return r.AddSlot(mongodb.AvailableSlotV2{
-			Id:        s.SlotId.String(),
-			DayId:     s.DayId,
-			Date:      s.StartTime.Format("02-01-2006"),
-			StartTime: s.StartTime.Format("15:04:05"),
-			Duration:  s.Duration})
+		return nil
 	})
 
 	p.When(events.SlotBooked{}, func(e interface{}, m infrastructure.EventMetadata) error {
-		b := e.(events.SlotBooked)
-		return r.HideSlot(b.SlotId)
+		return nil
 	})
 
-	p.When(events.SlotBookingCancelled{}, func(e interface{}, m infrastructure.EventMetadata) error {
-		c := e.(events.SlotBookingCancelled)
-		return r.ShowSlot(c.SlotId)
-	})
-
-	p.When(events.SlotScheduleCancelled{}, func(e interface{}, m infrastructure.EventMetadata) error {
-		c := e.(events.SlotScheduleCancelled)
-		return r.DeleteSlot(c.SlotId)
-	})
+	// Add when for SlotBookingCancelled
 
 	return &AvailableSlotsProjectionV2{p}
 }
